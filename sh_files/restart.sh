2407e7a80630d9a1bbdd64134fbe4d042f56e6b5
Abdulkadir <84346396+kadirculha@users.noreply.github.com>
2024-10-28 14:57:45 +0300

    Update restart.sh

--- sh_files/restart.sh
+++ sh_files/restart.sh
@@ -1,5 +1,5 @@
 docker rm -f corridor
 #docker run -d -it -v ":/code/Corridors" --name corridor -p 8080:8080 corridor
-docker run -d -it -v "/home/ubuntu/Volume/Corridors:/code/Corridors" --name corridor -p 8000:8000 corridor
+docker run -d -it -v "/home/ubuntu/Volume/Corridors:/code/Corridors" --name corridor -p 8080:8080 corridor
 
 # "C:\Users\apoks\OneDrive\Masaüstü\AllDataFromUbuntu\work\AiThinks\Volume\Corridors"
